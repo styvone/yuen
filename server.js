66501d0cf48aabcd2179345d5a3ed25b2f134db2
Steven La <stevenla@g.ucla.edu>
2018-08-06 17:28:25 -0700

    set up audio streaming from server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var youtubeStream = require('youtube-audio-stream');
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/stream/:videoId', function (req, res) {
+    try {
+        youtubeStream(req.params.videoId).pipe(res);
+    } catch (exception) {
+        res.status(500).send(exception)
+    }
+});
+
+app.listen(3000, function () {
+  console.log('app is listening on port 3000!');
+});
