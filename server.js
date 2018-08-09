377da5fa94a2ef59fbf7fc776d6891690f74d170
Steven La <stevenla@g.ucla.edu>
2018-08-09 14:12:45 -0700

    must be

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var youtubeStream = require('youtube-audio-stream');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
-
-var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
@@ -12,9 +13,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/stream/:videoId', function (req, res) {
-    youtubeStream(req.params.videoId).pipe(res);
+    youtubeStream(req.url.slice(1)).pipe(res);
 });
 
-app.listen(port, function () {
-  console.log('app is listening on port 3000!');
-});
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log('open http://localhost:3000 for demo of audio stream')
+})
