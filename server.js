f50ea37316bd94281375e2ad4290de765697a05e
Steven La <stevenla@g.ucla.edu>
2018-08-10 08:33:29 -0700

    yes

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-/*
 app.get('/stream/:videoId', function (req, res) {
-    try {
-    	youtubeStream(req.params.videoId).pipe(res);
-    } catch (exception) {
-        res.status(500).send(exception);
-    }
+
+	var id = req.params.videoId;
+	console.log('1');
+    var streaming = youtubeStream(id);
+    console.log('2');
+    var pipeIt = streaming.pipe(res);
+    console.log('3');
 });
-*/
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('app is listening on port 3000!');
