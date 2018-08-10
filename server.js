517cf52c073001d00f002bc492e7da117fed29b9
Steven La <stevenla@g.ucla.edu>
2018-08-09 17:37:19 -0700

    michael

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var youtubeStream = require('youtube-audio-stream');
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
 
 var app = express();
 
@@ -12,10 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/:videoId', function (req, res) {
-    youtubeStream(req.url.slice(1)).pipe(res);
+app.get('/stream/:videoId', function (req, res) {
+    try {
+    	youtubeStream(req.params.videoId).pipe(res);
+    } catch (exception) {
+        res.status(500).send(exception);
+    }
 });
 
-var server = app.listen(process.env.PORT || 3000, function() {
-  console.log('open http://localhost:3000 for demo of audio stream')
-})
+app.listen(process.env.PORT || 3000, function () {
+  console.log('app is listening on port 3000!');
+});
