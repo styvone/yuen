a6055dc300d763d3d4e206bc5e33bd5c60016831
Steven La <stevenla@g.ucla.edu>
2018-08-09 17:57:15 -0700

    no

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/stream/:videoId', function (req, res) {
     try {
+    	
     	youtubeStream(req.params.videoId).pipe(res);
     } catch (exception) {
         res.status(500).send(exception);
